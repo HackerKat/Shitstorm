e93c4ecc4ee770e6ce05ffd6f2448df12c2f2165
Leonid Barsht <leonidb93@gmail.com>
2023-10-28 17:39:33 +0200

    continue with distance script

--- Scripts/distanceChecker.gd
+++ Scripts/distanceChecker.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@onready var player = get_node("Player")
-@onready var tornado = get_node("Tornado")
+@onready var player = get_node("Demo/Player")
+@onready var tornado = get_node("Demo/Tornado")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +11,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	print(returnDistance())
+
+func returnDistance() -> float:
+	if !player: return -1
+	if !tornado: return -2
+	var position1 = player.global_position
+	var position2 = tornado.global_position
+	return position1.distance_to(position2)
